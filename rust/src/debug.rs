e232ded9bfc5dd7a2ebe479d5618e572d8b1a3ff
sisso <sisso@sisso.com.br>
2019-09-03 21:48:03 +0200

    Adding context

---
+++ rust/src/debug.rs
@@ -0,0 +1,24 @@
+#[macro_export]
+macro_rules! debug {
+    () => (debug!(""));
+    ($fmt:expr) => (match ::std::fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("/tmp/ffi.log") {
+            Ok(mut file) => {
+                use std::io::Write;
+                file.write_all(format!("{}\n", $fmt).as_bytes()).ok();
+            }
+            Err(_) => {},
+        });
+    ($fmt:expr, $($arg:tt)*) => (match ::std::fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("/tmp/ffi.log") {
+            Ok(mut file) => {
+                use std::io::Write;
+                file.write_all(format!(concat!($fmt, "\n"), $($arg)*).as_bytes()).ok();
+            }
+            Err(_) => {},
+        });
+}
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,92 @@
+#[macro_use]
+mod debug;
+
+use std::ffi::CStr;
+use std::mem::{drop, transmute};
+use std::net::UdpSocket;
+use std::os::raw::c_char;
+use std::ptr;
+use std::result::Result;
+
+pub struct Context {
+    input: i32,
+    output: u32,
+}
+
+impl<'a> Context {
+    fn new() -> Self {
+        Context {
+            input: 0,
+            output: 0,
+        }
+    }
+
+    fn to_ptr(self) -> *mut Context {
+        unsafe { transmute(Box::new(self)) }
+    }
+
+    fn from_ptr(ptr: *mut Context) -> &'a mut Context {
+        unsafe { &mut *ptr }
+    }
+
+    fn close(ptr: *mut *mut Context) {
+        let b: Box<Context> = unsafe { transmute(ptr) };
+        drop(b);
+    }
+
+    fn set_input(&mut self, value: i32) -> Result<(), String> {
+        if value < 0 {
+            Err(format!("invalid input, value can not be lower that 0"))
+        } else {
+            self.input = value;
+            Ok(())
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn context_create(ptr: *mut *const Context) -> bool {
+    let ctx = Context::new();
+
+    unsafe {
+        *ptr = ctx.to_ptr();
+    }
+
+    // if failed
+    // *ptr = ptr::null; false
+
+    true
+}
+
+#[no_mangle]
+pub extern "C" fn context_close(ptr: *mut *mut Context) {
+    if !ptr.is_null() && unsafe {!(*ptr).is_null() } {
+        Context::close(ptr);
+
+        unsafe {
+            *ptr = ptr::null_mut();
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn context_set_input(ptr: *mut Context, value: i32) -> bool {
+    if ptr.is_null() {
+        false
+    } else {
+        match Context::from_ptr(ptr).set_input(value) {
+            Ok(_) => {
+                debug!("setting input as {}", value);
+                true
+            },
+            Err(msg) => {
+                debug!(msg);
+                false
+            },
+        }
+    }
+}
+
 #[repr(C)]
 pub struct V2 {
     pub x: i32,
